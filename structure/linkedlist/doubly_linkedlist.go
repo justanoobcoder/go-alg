f20104585268c7e3311ad3c7eb830fa15934f473
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-12-02 22:19:04 +0700

    added reverse operation and test for doubly linkedlist

--- structure/linkedlist/doubly_linkedlist.go
+++ structure/linkedlist/doubly_linkedlist.go
@@ -44,3 +44,130 @@ func NewDoubly(items ...int) *DoublyLinkedList {
 func (l *DoublyLinkedList) Size() int {
 	return l.size
 }
+
+func (l *DoublyLinkedList) InsertAt(index, val int) error {
+	if index < 0 {
+		return ErrInvalidIndex
+	} else if index > l.size {
+		return ErrIndexOutOfRange
+	}
+	node := NewDoublyNode(val, nil, nil)
+	if index == 0 {
+		if l.size == 0 {
+			l.Head = node
+		} else {
+			node.Next = l.Head
+			l.Head.Prev = node
+			l.Head = node
+		}
+	} else {
+		curr := l.Head
+		for index > 1 {
+			curr = curr.Next
+			index--
+		}
+		node.Next = curr.Next
+		if curr.Next != nil {
+			curr.Next.Prev = node
+		}
+		node.Prev = curr
+		curr.Next = node
+	}
+	l.size++
+
+	return nil
+}
+
+func (l *DoublyLinkedList) BeginInsert(val int) {
+	l.InsertAt(0, val)
+}
+
+func (l *DoublyLinkedList) LastInsert(val int) {
+	l.InsertAt(l.size, val)
+}
+
+func (l *DoublyLinkedList) DeleteAt(index int) error {
+	if index < 0 {
+		return ErrInvalidIndex
+	} else if index >= l.size {
+		return ErrIndexOutOfRange
+	}
+	if index == 0 {
+		if l.size == 1 {
+			l.Head = nil
+		} else {
+			l.Head = l.Head.Next
+			l.Head.Prev = nil
+		}
+	} else {
+		curr := l.Head
+		for index > 1 {
+			curr = curr.Next
+			index--
+		}
+		curr.Next = curr.Next.Next
+		if curr.Next != nil {
+			curr.Next.Prev = curr
+		}
+	}
+	l.size--
+	return nil
+}
+
+func (l *DoublyLinkedList) BeginDelete() {
+	l.DeleteAt(0)
+}
+
+func (l *DoublyLinkedList) LastDelete() {
+	l.DeleteAt(l.size - 1)
+}
+
+func (l *DoublyLinkedList) GetValueAt(index int) (int, error) {
+	if index < 0 {
+		return 0, ErrInvalidIndex
+	} else if index >= l.size {
+		return 0, ErrIndexOutOfRange
+	}
+	curr := l.Head
+	for index > 0 {
+		curr = curr.Next
+		index--
+	}
+
+	return curr.Val, nil
+}
+
+func (l *DoublyLinkedList) Sort() {
+	if l.size <= 1 {
+		return
+	}
+	swapped := true
+	for swapped {
+		swapped = false
+		for curr := l.Head; curr.Next != nil; curr = curr.Next {
+			if curr.Val > curr.Next.Val {
+				swapped = true
+				curr.Val, curr.Next.Val = curr.Next.Val, curr.Val
+			}
+		}
+	}
+}
+
+func (l *DoublyLinkedList) Reverse() {
+	if l.size <= 1 {
+		return
+	}
+	var prev, curr, next *doublynode
+	curr = l.Head
+	next = curr.Next
+	for curr != nil {
+		curr.Next = prev
+		curr.Prev = next
+		prev = curr
+		curr = next
+		if next != nil {
+			next = next.Next
+		}
+	}
+	l.Head = prev
+}
--- structure/linkedlist/doubly_linkedlist_test.go
+++ structure/linkedlist/doubly_linkedlist_test.go
@@ -1,6 +1,7 @@
 package linkedlist_test
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 
@@ -38,6 +39,459 @@ func TestNewDoublyLinkedList(t *testing.T) {
 			}
 			if !reflect.DeepEqual(test.expected, got) {
 				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublyInsertAt(t *testing.T) {
+	table := []struct {
+		name        string
+		input       []int
+		index       int
+		val         int
+		expected    []int
+		expectedErr error
+	}{
+		{
+			name:        "insert at the beginning of an empty doubly linkedlist",
+			input:       []int{},
+			index:       0,
+			val:         1,
+			expected:    []int{1},
+			expectedErr: nil,
+		},
+		{
+			name:        "insert at the beginning of a non-empty doubly linkedlist",
+			input:       []int{2, 3, 4, 5},
+			index:       0,
+			val:         1,
+			expected:    []int{1, 2, 3, 4, 5},
+			expectedErr: nil,
+		},
+		{
+			name:        "insert at the end of a non-empty doubly linkedlist",
+			input:       []int{1, 2, 3, 4},
+			index:       4,
+			val:         5,
+			expected:    []int{1, 2, 3, 4, 5},
+			expectedErr: nil,
+		},
+		{
+			name:        "insert at the middle of a non-empty doubly linkedlist",
+			input:       []int{1, 2, 4, 5},
+			index:       2,
+			val:         3,
+			expected:    []int{1, 2, 3, 4, 5},
+			expectedErr: nil,
+		},
+		{
+			name:        "insert at the end of an empty doubly linkedlist",
+			input:       []int{},
+			index:       1,
+			val:         1,
+			expected:    []int{},
+			expectedErr: linkedlist.ErrIndexOutOfRange,
+		},
+		{
+			name:        "insert with negative index",
+			input:       []int{},
+			index:       -1,
+			val:         1,
+			expected:    []int{},
+			expectedErr: linkedlist.ErrInvalidIndex,
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			err := list.InsertAt(test.index, test.val)
+			got := make([]int, 0)
+			for curr := list.Head; curr != nil; curr = curr.Next {
+				got = append(got, curr.Val)
+			}
+			if test.expectedErr == nil && err != nil {
+				t.Errorf("want no error, got %v", err)
+			}
+			if test.expectedErr != nil && err == nil {
+				t.Errorf("want error %v, got no error", test.expectedErr)
+			}
+			if len(test.expected) != list.Size() {
+				t.Errorf("want size %v, got size %v", len(test.expected), list.Size())
+			}
+			if !reflect.DeepEqual(test.expected, got) {
+				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublyBeginInsert(t *testing.T) {
+	table := []struct {
+		name     string
+		input    []int
+		val      int
+		expected []int
+	}{
+		{
+			name:     "insert at the beginning of an empty doubly linkedlist",
+			input:    []int{},
+			val:      1,
+			expected: []int{1},
+		},
+		{
+			name:     "insert at the beginning of a non-empty doubly linkedlist",
+			input:    []int{2, 3, 4, 5},
+			val:      1,
+			expected: []int{1, 2, 3, 4, 5},
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			list.BeginInsert(test.val)
+			got := make([]int, 0)
+			for curr := list.Head; curr != nil; curr = curr.Next {
+				got = append(got, curr.Val)
+			}
+			if len(test.expected) != list.Size() {
+				t.Errorf("want size %v, got size %v", len(test.expected), list.Size())
+			}
+			if !reflect.DeepEqual(test.expected, got) {
+				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublyLastInsert(t *testing.T) {
+	table := []struct {
+		name     string
+		input    []int
+		val      int
+		expected []int
+	}{
+		{
+			name:     "insert at the end of an empty doubly linkedlist",
+			input:    []int{},
+			val:      1,
+			expected: []int{1},
+		},
+		{
+			name:     "insert at the end of a non-empty doubly linkedlist",
+			input:    []int{1, 2, 3, 4},
+			val:      5,
+			expected: []int{1, 2, 3, 4, 5},
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			list.LastInsert(test.val)
+			got := make([]int, 0)
+			for curr := list.Head; curr != nil; curr = curr.Next {
+				got = append(got, curr.Val)
+			}
+			if len(test.expected) != list.Size() {
+				t.Errorf("want size %v, got size %v", len(test.expected), list.Size())
+			}
+			if !reflect.DeepEqual(test.expected, got) {
+				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublyDeleteAt(t *testing.T) {
+	table := []struct {
+		name        string
+		input       []int
+		index       int
+		expected    []int
+		expectedErr error
+	}{
+		{
+			name:        "delete at the beginning of a non-empty doubly linkedlist",
+			input:       []int{1, 2, 3, 4},
+			index:       0,
+			expected:    []int{2, 3, 4},
+			expectedErr: nil,
+		},
+		{
+			name:        "delete at the end of a non-empty doubly linkedlist",
+			input:       []int{1, 2, 3, 4},
+			index:       3,
+			expected:    []int{1, 2, 3},
+			expectedErr: nil,
+		},
+		{
+			name:        "delete at the middle of a non-empty doubly linkedlist",
+			input:       []int{1, 2, 3, 4},
+			index:       2,
+			expected:    []int{1, 2, 4},
+			expectedErr: nil,
+		},
+		{
+			name:        "delete at the beginning of an empty doubly linkedlist",
+			input:       []int{},
+			index:       0,
+			expected:    []int{},
+			expectedErr: linkedlist.ErrIndexOutOfRange,
+		},
+		{
+			name:        "delete with negative index",
+			input:       []int{},
+			index:       -1,
+			expected:    []int{},
+			expectedErr: linkedlist.ErrInvalidIndex,
+		},
+		{
+			name:        "delete with index out of range",
+			input:       []int{1, 2, 3, 4},
+			index:       4,
+			expected:    []int{1, 2, 3, 4},
+			expectedErr: linkedlist.ErrIndexOutOfRange,
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			err := list.DeleteAt(test.index)
+			got := make([]int, 0)
+			for curr := list.Head; curr != nil; curr = curr.Next {
+				got = append(got, curr.Val)
+			}
+			if test.expectedErr == nil && err != nil {
+				t.Errorf("want no error, got %v", err)
+			}
+			if test.expectedErr != nil && err == nil {
+				t.Errorf("want error %v, got no error", test.expectedErr)
+			}
+			if test.expectedErr != nil && !errors.Is(err, test.expectedErr) {
+				t.Errorf("want error %v, got error %v", test.expectedErr, err)
+			}
+			if len(test.expected) != list.Size() {
+				t.Errorf("want size %v, got size %v", len(test.expected), list.Size())
+			}
+			if !reflect.DeepEqual(test.expected, got) {
+				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublyBeginDelete(t *testing.T) {
+	table := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "delete at the beginning of a non-empty doubly linkedlist",
+			input:    []int{1, 2, 3, 4},
+			expected: []int{2, 3, 4},
+		},
+		{
+			name:     "delete at the beginning of an empty doubly linkedlist",
+			input:    []int{},
+			expected: []int{},
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			list.BeginDelete()
+			got := make([]int, 0)
+			for curr := list.Head; curr != nil; curr = curr.Next {
+				got = append(got, curr.Val)
+			}
+			if len(test.expected) != list.Size() {
+				t.Errorf("want size %v, got size %v", len(test.expected), list.Size())
+			}
+			if !reflect.DeepEqual(test.expected, got) {
+				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublyLastDelete(t *testing.T) {
+	table := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "delete at the end of a non-empty doubly linkedlist",
+			input:    []int{1, 2, 3, 4},
+			expected: []int{1, 2, 3},
+		},
+		{
+			name:     "delete at the end of an empty doubly linkedlist",
+			input:    []int{},
+			expected: []int{},
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			list.LastDelete()
+			got := make([]int, 0)
+			for curr := list.Head; curr != nil; curr = curr.Next {
+				got = append(got, curr.Val)
+			}
+			if len(test.expected) != list.Size() {
+				t.Errorf("want size %v, got size %v", len(test.expected), list.Size())
+			}
+			if !reflect.DeepEqual(test.expected, got) {
+				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublyGetValueAt(t *testing.T) {
+	table := []struct {
+		name        string
+		input       []int
+		index       int
+		expected    int
+		expectedErr error
+	}{
+		{
+			name:        "get value at the beginning of a non-empty doubly linkedlist",
+			input:       []int{1, 2, 3, 4},
+			index:       0,
+			expected:    1,
+			expectedErr: nil,
+		},
+		{
+			name:        "get value at the end of a non-empty doubly linkedlist",
+			input:       []int{1, 2, 3, 4},
+			index:       3,
+			expected:    4,
+			expectedErr: nil,
+		},
+		{
+			name:        "get value at the middle of a non-empty doubly linkedlist",
+			input:       []int{1, 2, 3, 4},
+			index:       2,
+			expected:    3,
+			expectedErr: nil,
+		},
+		{
+			name:        "get value at the beginning of an empty doubly linkedlist",
+			input:       []int{},
+			index:       0,
+			expected:    0,
+			expectedErr: linkedlist.ErrIndexOutOfRange,
+		},
+		{
+			name:        "get value with negative index",
+			input:       []int{},
+			index:       -1,
+			expected:    0,
+			expectedErr: linkedlist.ErrInvalidIndex,
+		},
+		{
+			name:        "get value with index out of range",
+			input:       []int{1, 2, 3, 4},
+			index:       4,
+			expected:    0,
+			expectedErr: linkedlist.ErrIndexOutOfRange,
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			got, err := list.GetValueAt(test.index)
+			if test.expectedErr == nil && err != nil {
+				t.Errorf("want no error, got %v", err)
+			}
+			if test.expectedErr != nil && err == nil {
+				t.Errorf("want error %v, got no error", test.expectedErr)
+			}
+			if test.expectedErr != nil && !errors.Is(err, test.expectedErr) {
+				t.Errorf("want error %v, got error %v", test.expectedErr, err)
+			}
+			if test.expected != got {
+				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublySort(t *testing.T) {
+	table := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "sort a non-empty doubly linkedlist",
+			input:    []int{4, 2, 3, 1},
+			expected: []int{1, 2, 3, 4},
+		},
+		{
+			name:     "sort an empty doubly linkedlist",
+			input:    []int{},
+			expected: []int{},
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			list.Sort()
+			got := make([]int, 0)
+			for curr := list.Head; curr != nil; curr = curr.Next {
+				got = append(got, curr.Val)
+			}
+			if !reflect.DeepEqual(test.expected, got) {
+				t.Errorf("want %v, got %v", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestDoublyReverse(t *testing.T) {
+	table := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "reverse a non-empty doubly linkedlist",
+			input:    []int{1, 2, 3, 4},
+			expected: []int{4, 3, 2, 1},
+		},
+		{
+			name:     "reverse an empty doubly linkedlist",
+			input:    []int{},
+			expected: []int{},
+		},
+	}
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			list := linkedlist.NewDoubly(test.input...)
+			list.Reverse()
+			reversedList := make([]int, 0)
+			originList := make([]int, 0)
+			curr := list.Head
+			for curr != nil && curr.Next != nil {
+				reversedList = append(reversedList, curr.Val)
+				curr = curr.Next
+			}
+			if curr != nil {
+				reversedList = append(reversedList, curr.Val)
+			}
+			for ; curr != nil; curr = curr.Prev {
+				originList = append(originList, curr.Val)
+			}
+			if !reflect.DeepEqual(test.input, originList) {
+				t.Errorf("want %v, originList %v", test.input, originList)
+			}
+			if !reflect.DeepEqual(test.expected, reversedList) {
+				t.Errorf("want %v, reversedList %v", test.expected, reversedList)
 			}
 		})
 	}
