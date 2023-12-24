b9e37d02eaf936964e8a821f1ae7603b30cca353
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-12-24 18:45:17 +0700

    optimized bubble sort

--- sort/bubble_sort.go
+++ sort/bubble_sort.go
@@ -2,17 +2,21 @@ package sort
 
 // BubbleSort
 // Worst case: O(n^2)
-// Best case: O(n)
+// Best case: O(n) (when dataset's already sorted)
+// Avg case: O(n^2)
 func BubbleSort(arr []int) []int {
+	n := len(arr)
 	swapped := true
 	for swapped {
 		swapped = false
-		for i := 0; i < len(arr)-1; i++ {
-			if arr[i] > arr[i+1] {
-				arr[i], arr[i+1] = arr[i+1], arr[i]
+		for i := 1; i < n; i++ {
+			if arr[i-1] > arr[i] {
+				arr[i-1], arr[i] = arr[i], arr[i-1]
 				swapped = true
 			}
 		}
+		n--
 	}
+
 	return arr
 }
