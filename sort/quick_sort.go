5c5a93d5e49f8e073b0d141470fa5ae5b944ebd9
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-12-24 19:11:07 +0700

    re-wrote quick sort, avoid O(n^2) case

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -1,39 +1,53 @@
 package sort
 
+// QuickSort
+// Best case: O(nlog(n))
+// Worst case: O(n^2) (when dataset's already sorted,
+//    but can be avoided and reduced to O(nlog(n)) )
+// Avg: O(nlog(n))
+func QuickSort(arr []int) []int {
+	quickSortWithRange(arr, 0, len(arr)-1)
+	return arr
+}
+
 func partition(arr []int, start, end int) int {
-	pivot := arr[end]
-	p := start - 1
-
-	for i := start; i < end; i++ {
-		if arr[i] <= pivot {
-			p++
-			arr[i], arr[p] = arr[p], arr[i]
-		}
+	mid := (start + end) / 2
+	if arr[mid] < arr[start] {
+		arr[mid], arr[start] = arr[start], arr[mid]
 	}
-	p++
-	arr[p], arr[end] = arr[end], arr[p]
-
-	return p
+	if arr[end] < arr[start] {
+		arr[end], arr[start] = arr[start], arr[end]
+	}
+	if arr[mid] < arr[end] {
+		arr[mid], arr[end] = arr[end], arr[mid]
+	}
+	pivot := arr[end]
+	i := start - 1
+	j := end + 1
+	for {
+		for {
+			i++
+			if arr[i] >= pivot {
+				break
+			}
+		}
+		for {
+			j--
+			if arr[j] <= pivot {
+				break
+			}
+		}
+		if i >= j {
+			return j
+		}
+		arr[i], arr[j] = arr[j], arr[i]
+	}
 }
 
 func quickSortWithRange(arr []int, start, end int) {
-	if len(arr) <= 1 {
-		return
-	}
 	if start < end {
 		p := partition(arr, start, end)
-		quickSortWithRange(arr, start, p-1)
+		quickSortWithRange(arr, start, p)
 		quickSortWithRange(arr, p+1, end)
 	}
-}
-
-// QuickSort
-// Best case: O(nlog(n))
-// Worst case: O(n^2)
-// Avg: O(nlog(n))
-func QuickSort(arr []int) []int {
-	if len(arr) > 1 {
-		quickSortWithRange(arr, 0, len(arr)-1)
-	}
-	return arr
 }
