155c97b393b076f3e4ec9cb481c9251d20a47384
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-12-24 18:55:42 +0700

    added avg case for selection sort

--- sort/selection_sort.go
+++ sort/selection_sort.go
@@ -3,10 +3,12 @@ package sort
 // SelectionSort
 // Worst case: O(n^2)
 // Best case: O(n^2)
+// Avg case: O(n^2)
 func SelectionSort(arr []int) []int {
-	for i := 0; i < len(arr)-1; i++ {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
 		minIdx := i
-		for j := i + 1; j < len(arr); j++ {
+		for j := i + 1; j < n; j++ {
 			if arr[j] < arr[minIdx] {
 				minIdx = j
 			}
